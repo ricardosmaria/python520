ad09482c66d3de4be044b8242989d62f24a27e5d
ricardosmaria <ricardo_maria@metrosp.com.br>
2019-11-25 18:04:16 -0200

    Aula1

---
+++ aula1/listasTuplasDicionario.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python3
+
+# #contagem de lista começa sempre do 0
+# lista = ['Arroz','Feijão','Óleo','Sal','Açucar','Temperos','Cerveja']
+# #           0       1       2       3       4       5
+# #vetores multidimensionais 
+# lista3d = [
+#     [ 2 , 3 , 4 , 5 ],
+#     [ 3 , 4 , 5 , 6 ],
+#     [ 7 , 5 , 7 , 8],
+# ]
+# #add un item no final da lista
+# lista.append('Carne')
+# #adicionar un item no determinado indice da lista (caso contrario indice 0)
+# lista.insert(4,'Sabão em pó')
+
+# print(lista)
+# #remove o o elemento pelo id
+# # lista.pop(4)
+
+# #ordena a lista em, ordem alfabetica/ numerica
+# lista.sort()
+
+# #reverte a ordem da lista
+# lista.reverse()
+
+# #mostra quantidade de elementos
+# print(lista)
+
+# #mostra qtdade de elementos 
+# print(len(lista))
+
+# #procura o primeiro indice da ocorrencia
+# print(lista.index('Açucar'))
+
+# #conta qtde de indices x no array
+# print(lista.count('Arroz'))
+
+# #conta qtde de indices da ocorrencia com o seguinte valor
+# #lista.remove('Sabao em pó')
+# print(lista)
+
+# lista[3] = 'Maça'
+# print (lista)
+
+############
+### tupla###
+############
+
+# #primeiro metodo para criar tuplas
+# tupla = ('maça','banana','laranja','abacaxi','uva',
+#              'melao','melancia','limao','morango',
+#              'lichia','mixirica','manga')
+
+# #seghhundo metodo para criar tuplas
+# tupla2 = 'valor1' , 2, True, 2j
+# print(type(tupla2))
+
+# #acessando indices de uma tupla
+# print(tupla[3])
+# print(tupla[1:4])
+# print(tupla[-2])
+
+# #converter tupla para lista
+# lista_da_tupla = list(tupla)
+# print(type(lista_da_tupla))
+
+# print(tupla)
+
+# tupla = (['indice 1'], 'nome')
+# tupla[0].insert(1, 'indice 2')
+# print(tupla)
+
+####################
+#####diconarios#####
+####################
+
+# #criando dicionario
+# apresentacoes = {
+#     'paulista' : 'salve',
+#     'carioca' : 'eae',
+#     'pirata' : 'argh',
+#     'mineiro' : 'pao de queijo',
+#     'acre' : 'barulhos de dinossauro',
+# }
+
+# #acessando indices em um dicionário
+# print(apresentacoes['paulista'])
+
+# #criando um dicionario com multi-valores internos
+# linguagem_favorita = {
+#     'daniel' : {
+#         'linguagem' : 'Phyton',
+#         'tempo_de_experiencia' : 5
+#     },
+#     'olimpio' : {
+#         'linguagem' : 'r',
+#         'linguagem' : 'vba', 
+#         'tempo_de_experiencia' : 10
+#     },
+# }
+
+# print(linguagem_favorita.get('daniel')['linguagem'])
+
+# #acesso a todas as chaves
+# print(linguagem_favorita.keys())
+
+# #acesso aos valores 
+# print(linguagem_favorita.values())
+# #acesso aos itens
+# print(valores.itens())
+
+
+letras = 'abcdefghjkil' + 'fgndsandnfakjfkjasfiuaehfi'
+print
+(float(input('Digite um numero: ')) + int(input("digite outro numero: ")))
+
+
