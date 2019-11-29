aca235dcc56448b1e5f3babab3caef20d0ef0db8
ricardosmaria <ricardo_maria@metrosp.com.br>
2019-11-29 17:40:52 -0200

    aula5

--- aula5/sql.py
+++ aula5/sql.py
@@ -10,11 +10,16 @@ try:
     con_psql = psycopg2.connect('host=localhost dbname=projeto user=admin password=4linux')
     cur = con_psql.cursor()
 
-    banco_psql = Banco(con_psql,cur)
+    banco_psql = Banco(cur_psql, con_psql)
 
     #conexao com mysql
     con_mysql = MySQLdb.connect(host='127.0.0.1',db='scripts',
                                 user='developer',passwd='4linux')
+
+
+    cur_mysql = con_mysql.cursor()
+    banco_psql = Banco(cur_mysql, con_mysql)
+
 except Exception as e:
     print(e)
     exit()
@@ -24,7 +29,10 @@ try:
     # print(banco_psql.seleciona_primeiro('s'))
     #banco_psql.delete('s','nome','Daniel')
     #print(banco_psql.seleciona_todos('s'))
-    pass
+
+    banco_mysql.inserir('Daniel','Programador')
+    print(banco_mysql.seleciona_todos('scipts'))
+    #pass
 except Exception as e:
     con_psql.rollback()
     con_mysql.rollback()
---
+++ aula5/teste.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# print('oi estou sendo executado')
+
+
+# import unittest
+
+# def soma(n1,n2):
+#     return n1 + n2
+
+# def subtrai(n1,n2):
+#     return n1 - n2
+
+# class Testes(unittest.TestCase):
+#     def teste_soma(self):
+#         self.assertEqual(soma(8,2),10)
+#         self.assertLess(soma(5,5),12)
+
+#     def testes_subtrai(self):
+#         self.assertEqual(subtrai(10,2), 8)
+#         self.assertGreater(subtrai(10,4),3)
+
+# if __name__ == '__main__':
+#     unittest.main()
+    #assert soma(3,2) == 5, "Módulo somar falhou"
+    #assert soma(3,5) == 5, "Módulo somar2 falhou"
+
+
+# trabalhar com mongo
